5274b31ea479f93e45860e777bc51cfa7506884a
Kaustav Deka <116963260+deka27@users.noreply.github.com>
2024-02-15 17:22:39 +0530

    Update navigation and theme configuration

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>FURY</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/',
   footer: {
     text: 'Nextra Docs Template',
   },
+  faviconGlyph: '📦',
+
 }
 
 export default config
